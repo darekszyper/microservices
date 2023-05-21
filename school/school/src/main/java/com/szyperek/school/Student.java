043f18be3013fda6a1f9721e6e8dbccedca14960
darekszyper <114878453+darekszyper@users.noreply.github.com>
2023-05-21 22:30:01 +0200

    Eureka configured, two microservices connected via FeignClient

---
+++ school/school/src/main/java/com/szyperek/school/Student.java
@@ -0,0 +1,15 @@
+package com.szyperek.school;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+}
---
+++ student/student/src/main/java/com/szyperek/student/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.szyperek.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findAllBySchoolId(Integer schoolId);
+}
